ab0a9bdd15b1d88b364c1443d1212ae8134b9091
Liberontissauri <jp.liberon@gmail.com>
2020-12-31 15:33:08 +0000

    added a website GUI for the game.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Rock-Paper-Scissors-Odin-Project
 A Rock Paper Scissors implementation for the Odin Project
+
+
+## Image Attributions:
+
+- Scissors by Cristiano Zoucas from the Noun Project;
+- Fist by Cristiano Zoucas from the Noun Project;
+- Hand by Cristiano Zoucas from the Noun Project;
--- index.js
+++ index.js
@@ -7,36 +7,96 @@ function computerPlay () {
         case 1:
             return "Scissors";
         case 2:
-            return "Stone";
+            return "Rock";
         default:
             return "A Problem Ocurred"
     }
     
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection, computerSelection) { // Returns 0 on loss; 1 on win and 2 on tie
     if (
-        (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "stone")||
-        (playerSelection.toLowerCase() == "stone" && computerSelection.toLowerCase() == "scissors")||
+        (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock")||
+        (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors")||
         (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper")
     ) {
-        return `Ỳou win, ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}!!`;
+        return 1;
 
     } else if (
-        (playerSelection.toLowerCase() == "stone" && computerSelection.toLowerCase() == "paper")||
-        (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "stone")||
+        (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper")||
+        (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock")||
         (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors")
     ) {
-        return `Ỳou lose, ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}...`;
+        return 0;
     } else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        return `It's a tie! You both chose ${playerSelection.toLowerCase()}.`;
+        return 2;
     }
 }
 
 function game () {
-    for (i=0; i<5; i++) {
-        console.log(playRound(prompt("Make your choice:", "paper"), computerPlay()));
+    const rockBtn = document.querySelector("#RockButton");
+    const paperBtn = document.querySelector("#PaperButton");
+    const scissorsBtn = document.querySelector("#ScissorsButton");
+
+    rockBtn.addEventListener("click", () =>  {
+        updateDisplay ("Rock", computerPlay());
+    })
+
+    paperBtn.addEventListener("click", () =>  {
+        updateDisplay ("Paper", computerPlay());
+    })
+
+    scissorsBtn.addEventListener("click", () =>  {
+        updateDisplay ("Scissors", computerPlay());
+    })
+}
+
+function updateDisplay (playerSelection, computerSelection) {
+    const playerSquare = document.querySelector("#PlayerSelection");
+
+    const computerSquare = document.querySelector("#ComputerSelection")
+
+    const ChoiceText = document.querySelector("#ChoiceText");
+
+    playerSquare.style.visibility = "visible";
+    computerSquare.style.visibility = "visible";
+    
+    switch (playerSelection) {
+        case "Paper":
+            playerSquare.src = "images/paper_symbol.png";
+            break;
+        case "Rock":
+            playerSquare.src = "images/rock_symbol.png";
+            break;
+        case "Scissors":
+            playerSquare.src = "images/scissors_symbol.png";
+            break;
     }
+    switch (computerSelection) {
+        case "Paper":
+            computerSquare.src = "images/paper_symbol.png";
+            break;
+        case "Rock":
+            computerSquare.src = "images/rock_symbol.png";
+            break;
+        case "Scissors":
+            computerSquare.src = "images/scissors_symbol.png";
+            break;
+    }
+
+    switch (playRound(playerSelection, computerSelection)){
+        case 0:
+            ChoiceText.textContent = "You Lost";
+            break;
+        case 1:
+            ChoiceText.textContent = "You Won";
+            break;
+        case 2:
+            ChoiceText.textContent = "It's a Tie";
+            break;
+    }
+
+    setTimeout(() => ChoiceText.textContent = "Make your choice:", 1000);
 }
 
 game()
